92eaf68ad9c5b68d7fd888c9ca157ce54219d49f
Deem97 <thdeema@gmail.com>
2025-06-09 13:47:25 +0530

    Fix Login Issue

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:3001', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-Requested-With'], // Added X-Requested-With
 }));
 
 // Basic middleware
